4816ac7bf4d22156cab1807e93f891f73237ff68
zih <74742492+zihoffman7@users.noreply.github.com>
2025-04-16 13:05:44 -0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import MyApp from "./MyApp";
-import "./index.css";
-
-const container = document.getElementById("root");
-const root = ReactDOMClient.createRoot(container);
-
-root.render(<MyApp />);
